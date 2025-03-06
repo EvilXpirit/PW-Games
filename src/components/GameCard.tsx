9c4eb4e30f0dec2197cfc76590599c7d83797b95
ADitya SHarma <adsharma122@gmail.com>
2025-03-07 02:43:30 +0530

    second commit

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Game } from '../types';
 import Link from 'next/link';
-import Image from 'next/image';
+// import Image from 'next/image';
 
 interface GameCardProps {
   game: Game;
